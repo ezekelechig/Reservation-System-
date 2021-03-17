f1f283daec153d4cafff8a7c261885d90177e7be
Kelechi Eze <kelechigodwin9@gmail.com>
2021-03-16 22:23:03 -0500

    Started Implementing Linked List data structure for the project

--- Documentation_folder/Reservation_System.cpp
+++ Documentation_folder/Reservation_System.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <array>
 
 using namespace std;
 
@@ -10,21 +11,19 @@ class Flight{
         string arrival_city; 
         string airport;
         string destination;
-        std::string cabinclass[3];
+        string cabinclass[3];
+        
 
     public:
         string name;
         int number;
 
-        // Flight()
-        // {
-        //     cabinclass = NULL;
-        // }
+        Flight(){};
 
         Flight(string name, int number)
         {
             this->name = name;
-            this->name = number;
+            this->number = number;  //error in assignment
         }
     
     void setdeparture_city(string _city)
@@ -68,20 +67,107 @@ class Flight{
 
   void setcabinclass()
   {
-      cabinclass[0] = "Economy";
-      cabinclass[1] = "FirstClass";
-      cabinclass[2] = "Business";
+      this->cabinclass[0] = "Economy";
+      this->cabinclass[1] = "FirstClass";
+      this->cabinclass[2] = "Business";
 
   }
 
+ string getcabinclass(string _cclass)
+  {
+      for(int i = 0; i < 3; i++)
+      {
+          if(this->cabinclass[i] == _cclass) return _cclass;
+      }
+      return "Invalid input";
+    }
+
+};
+    std::ostream& operator << (std:: ostream &out, Flight& data)
+    {
+        out << data.name << ": ";
+        out << data.number << ": ";
+        out << data.getdeparture_city()<< ": ";
+        out << data.getcabinclass("Economy") << ": ";
+        return out;
+    }
+//::::::::::::::::::::::::::::::BEGINS::::::::::::::LINKEDLIST IMPLEMENTATION::::::::::::::::::::::::::::::::::::
+struct Node
+{
+    Flight data;
+    struct Node* next;
+};
+
+class FlightLinkedList // : Flight
+{
+    private:
+        Node* head;
+        Node* tail;
+        int size;
+    public:
+        FlightLinkedList()
+        {
+            head = tail = NULL;
+            size = 0;
+        }
+        void addToList(Flight x)
+        {
+            Node* newFlight = new Node();
+            newFlight->data = x;
+            newFlight->next = NULL;
+            if(head == NULL)
+            {
+                head = tail = newFlight;
+            } 
+            else
+            {
+                tail->next = newFlight;
+                tail = newFlight;
+            }
+            size++;  
+        }
+        void getSize()
+        {
+            cout<< "Size of FlightLinkedList Data Store is: " << size <<endl;
+        }
+        void displayFlight()
+        {
+            Node*temp = head;
+            if(head == NULL) cout<<"List is Empty" <<endl;
+            else
+            {
+                cout<<"The FlightLinkedList Contains the Following Flight: " <<endl;
+                while(temp!= NULL)
+                {
+                    cout<< temp->data <<endl;
+                    temp = temp->next;
+                }
+            }
+        }
 
 };
 
+//::::::::::::::::::::::::::::::ENDS::::::::::::::::LINKEDLIST IMPLEMENTATION::::::::::::::::::::::::::::::::::::
+
 int main()
 {
-    Flight plain1("Spirit", 12345);
+    Flight plain1("SpiritAirline", 12345);
+    Flight plain2("AricAirline", 65876);
     plain1.setdeparture_city("Houston");
-    cout<<plain1.setcabinclass()<<endl;
+    plain2.setdeparture_city("Lagos-Nigeria");
+    plain1.setcabinclass();
+    cout<<plain1.name<<endl;
+    cout<<plain1.getcabinclass("Business")<<endl;
+
+ //::::::::::::::LINKEDLIST:::::::::::::::::::::::::
+    FlightLinkedList flightlist;
+    flightlist.addToList(plain1);
+    flightlist.addToList(plain2);
+    flightlist.displayFlight();
+    flightlist.getSize();
+    
+ //::::::::::::::END::::::::::::::::::::::::::::::::
+
 
     return 0;
 }
